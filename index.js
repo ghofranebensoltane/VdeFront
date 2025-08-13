d0f2060e3ddc7d6c5f6f552ab80c2edfa7275081
Ghofrane <BenSoltane.Ghofrane@esprit.tn>
2025-08-13 11:05:41 +0200

    Excercie 2

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ console.log('Nom: Ghofrane');
 console.log('Age: 26ans');
 
 alert('Bienvenue!');
+
+
+//////Excercice 2///////////
+let nom = "Ghofrane";
+let age =26; 
+console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);
+
+age = 27;
+console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);
