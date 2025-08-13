a6c40d04fcec9757e83f038e45cefdc4bfef4c15
Ghofrane <BenSoltane.Ghofrane@esprit.tn>
2025-08-13 11:29:38 +0200

    Excercice 4

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);*/
 
 ////Exercice 3//////////
 
-let nombre1= Number(prompt('Entrez un nombre : '));
+/*let nombre1= Number(prompt('Entrez un nombre : '));
 let nombre2= Number(prompt('Entrez un autre nombre : '));
 
 let somme = nombre1 + nombre2;
@@ -27,3 +27,41 @@ let produit = nombre1 * nombre2;
 console.log("la somme de deux nombre est : " +somme + 
     "\nla differnce de deux nombre est : " +difference +
     "\nle produit  de deux nombre est : " +produit); 
+*/
+
+///Excercice 4////
+
+/*let nombre = Number(prompt("Entrez un nombre"));
+
+if (nombre > 0){
+    console.log("le nombre que vous avez entrer " + nombre + " est positif");
+}
+else if(nombre < 0){
+    console.log("le nombre que vous avez entrer " + nombre + " est negatif");
+}
+else {
+    console.log("le nombre est null");
+}
+
+if (nombre % 2 ==0){
+    console.log("le nombre que vous avez entrer " + nombre + " est pair");
+}
+else {
+    console.log("le nombre que vous avez entrer " + nombre + " est impair");
+}*/
+
+//////Exercice 5/////////
+
+for (let i =0 ; i<= 10; i++){
+   console.log(i);
+}
+
+for (let i=0; i<=20; i++){
+    if (i %2==0){
+        console.log(i);
+    }
+}
+
+for (let i=0; i<=10; i++){
+    console.log("7 * " +i + " = " +(7*i));
+}
