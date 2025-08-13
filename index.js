8a5837f26f84412db5dadc5417950046674d36b0
Ghofrane <BenSoltane.Ghofrane@esprit.tn>
2025-08-13 11:13:48 +0200

    Excercice 3

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 //Excerice 1
-console.log('Bonjour javaScript');
+/*console.log('Bonjour javaScript');
 
 console.log('Nom: Ghofrane');
 console.log('Age: 26ans');
 
-alert('Bienvenue!');
+alert('Bienvenue!');*/
 
 
 //////Excercice 2///////////
-let nom = "Ghofrane";
+/*let nom = "Ghofrane";
 let age =26; 
 console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);
 
 age = 27;
-console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);
+console.log ("je m'appelle: " +nom + " " + "et j'ai: " +age);*/
+
+////Exercice 3//////////
+
+let nombre1= Number(prompt('Entrez un nombre : '));
+let nombre2= Number(prompt('Entrez un autre nombre : '));
+
+let somme = nombre1 + nombre2;
+let difference = nombre1 - nombre2;
+let produit = nombre1 * nombre2;
+
+console.log("la somme de deux nombre est : " +somme + 
+    "\nla differnce de deux nombre est : " +difference +
+    "\nle produit  de deux nombre est : " +produit); 
